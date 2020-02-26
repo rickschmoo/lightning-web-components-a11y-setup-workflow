30c36637f2f685db4d497ef1eb8fb49153d40bd8
Richard Boardman <rboardman@salesforce.com>
2020-02-25 16:37:43 -0800

    Example jest test

---
+++ HelloWorldLightningWebComponent2/force-app/main/default/lwc/helloworld/__tests__/helloworld.test.js
@@ -0,0 +1,24 @@
+// hello.test.js
+import { createElement } from 'lwc';
+import Helloworld from 'c/helloworld';
+
+describe('c-helloworld', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('displays greeting', () => {
+        // Create element
+        const element = createElement('c-helloworld', {
+            is: Helloworld
+        });
+        document.body.appendChild(element);
+
+        // Verify displayed greeting
+        const div = element.shadowRoot.querySelector('p');
+        expect(div.textContent).toMatch(/Hello/);
+    });
+});
